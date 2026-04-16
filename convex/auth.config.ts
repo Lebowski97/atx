1f551d6b2688e5c95d078f4692bb923d7e0e9083
Lebowski97 <joseph@tinytreesfarm.com>
2026-04-16 10:29:47 -0500

    deployment fix again

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: "https://full-sheep-1.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
