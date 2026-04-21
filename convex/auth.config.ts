267edbbd03091945f49c4ab7700b3885b3ef7b55
Lebowski97 <joseph@tinytreesfarm.com>
2026-04-21 00:46:30 -0500

    convex update

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,5 +4,9 @@ export default {
       domain: "https://full-sheep-1.clerk.accounts.dev",
       applicationID: "convex",
     },
+    {
+      domain: "https://clerk.hq.tinytreesatx.com",
+      applicationID: "convex",
+    },
   ],
 };
